acee77681d88e3a15ae6475c8de449cd45c45632
tangzhaoyang <123456>
2021-12-01 14:41:56 +0800

    gradle 7.0 版本

---
+++ app/src/main/java/com/tangzy/gradleplugin/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tangzy.gradleplugin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tangzy.gradleplugin.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
