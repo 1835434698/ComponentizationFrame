4cba23e7071d1b8b517f8f012b346df712e1faf2
tangzhaoyang <123456>
2021-12-01 11:19:51 +0800

    init

---
+++ myplugin/src/main/groovy/com/tangzy/plugin/MyPlugin.groovy
@@ -0,0 +1,17 @@
+package com.tangzy.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+public class MyPlugin implements Plugin<Project> {
+
+    void apply(Project project) {
+        def log = project.logger
+        log.error "========================";
+        log.error "完整的MyPlugin，开始修改Class!";
+        log.error "========================";
+        System.out.println("========================")
+        System.out.println( "完整的MyPlugin，开始修改Class!")
+        System.out.println("========================")
+    }
+}
